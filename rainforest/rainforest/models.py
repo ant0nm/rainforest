1ae1823abde6c206f952143f151f13f14f95c6b2
Anton <anton.mois33v@gmail.com>
2019-03-08 15:48:50 -0500

    Implemented validations for product description

--- rainforest/rainforest/models.py
+++ rainforest/rainforest/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
+
+min_length = MinLengthValidator(limit_value=10)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=128)
-    description = models.TextField()
+    description = models.TextField(max_length=500, validators=[min_length])
     price = models.IntegerField()
 
     def __str__(self):
--- rainforest/rainforest/views.py
+++ rainforest/rainforest/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from rainforest.models import Product
 from rainforest.forms import ProductForm
+from django.core.exceptions import ValidationError
 
 
 def root(request):
@@ -9,24 +10,17 @@ def root(request):
 
 
 def list_products(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    html_string = render(request, 'index.html', context)
-    return HttpResponse(html_string)
-
+    return render(request, 'index.html', {
+        'products': Product.objects.all()
+    })
 
 def show_product(request, id):
-    product = Product.objects.get(pk=id)
-    context = {'product': product}
-    html_string = render(request, 'product.html', context)
-    return HttpResponse(html_string)
+    return render(request, 'product.html', {
+        'product': Product.objects.get(pk=id)
+    })
 
 def new_product(request):
-    context = {
-        'form': ProductForm()
-    }
-    html_string = render(request, 'new.html', context)
-    return HttpResponse(html_string)
+    return render(request, 'new.html', {'form': ProductForm()})
 
 def create_product(request):
     form = ProductForm(request.POST)
@@ -34,4 +28,14 @@ def create_product(request):
         new_product = form.save()
         return HttpResponseRedirect('/')
     else:
-        print(form.errors)
+        return render(request, 'new.html', {
+            'form': form
+        })
+
+        # html_string = render(request, 'new.html' {'errors': form.errors})
+        # return HttpResponseRedirect(html_string)
+
+        # return render(request, 'new.html', {'errors': form.errors})
+        # context = {'errors': form.errors}
+        # html_string = render(request, 'new.html', context)
+        # return HttpResponseRedirect(html_string)
